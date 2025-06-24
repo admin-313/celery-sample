f46eeb5233747dc0e74df2be8d2feaf7c35a3cf7
admin-313 <w@quantum-level.net>
2025-06-24 17:20:36 +0300

    add celery provider

--- src/celery_sample/producer/bootstrap/containers.py
+++ src/celery_sample/producer/bootstrap/containers.py
@@ -1,6 +1,12 @@
+from celery import Celery
 from dependency_injector import containers, providers
 
 
 class Container(containers.DeclarativeContainer):
-
     wiring_config = containers.WiringConfiguration(modules=["producer.router"])
+    celery: providers.Resource[Celery] = providers.Resource(
+        Celery,
+        main="worker",
+        broker="pyamqp://guest:guest@172.20.0.2:5672//",
+        backend="redis://:RvPtXZyLRxRd7zvj3mieYvXAQAQH9Cjw@172.20.0.2:6379/0",
+    )
