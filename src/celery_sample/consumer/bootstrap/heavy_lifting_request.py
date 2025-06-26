181d0110e95052abc9afd7db3a6b49d378fe64ba
admin-313 <w@quantum-level.net>
2025-06-26 15:13:06 +0300

    add on_failure overwrite and heavy lifting task

--- src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
+++ src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
@@ -1,5 +1,21 @@
+import logging
+
+from celery import Task
 from celery.worker.request import Request
+
+logger = logging.getLogger(__name__)
 
 
 class HeavyLiftingRequest(Request):
-    pass
+    def on_failure(
+        self,
+        exc_info,
+        send_failed_event: bool = True,
+        return_ok: bool = False,
+    ) -> None:
+        super().on_failure(exc_info, send_failed_event, return_ok)
+        logger.error("The task %s has been failed", self.task.name)
+
+
+class HeavyLiftingTask(Task):
+    Request = HeavyLiftingRequest
