34abf3be84ba63ae01e2ac3b4fc238c4ba346042
admin-313 <w@quantum-level.net>
2025-06-26 15:14:27 +0300

    add ruff ignores

--- src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
+++ src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 class HeavyLiftingRequest(Request):
     def on_failure(
         self,
-        exc_info,
-        send_failed_event: bool = True,
-        return_ok: bool = False,
+        exc_info,  # noqa: ANN001
+        send_failed_event: bool = True,  # noqa: FBT001, FBT002
+        return_ok: bool = False,  # noqa: FBT001, FBT002
     ) -> None:
         super().on_failure(exc_info, send_failed_event, return_ok)
         logger.error("The task %s has been failed", self.task.name)
