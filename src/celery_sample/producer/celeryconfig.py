e884c60792c16052cb09bc3afe361702474a583a
admin-313 <w@quantum-level.net>
2025-06-27 11:21:50 +0300

    add type hints

--- src/celery_sample/producer/celeryconfig.py
+++ src/celery_sample/producer/celeryconfig.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True, slots=True)
 class CeleryConfig:
-    broker_url = "pyamqp://guest:guest@172.20.0.4:5672//"
-    result_backend = (
+    broker_url: str = "pyamqp://guest:guest@172.20.0.4:5672//"
+    result_backend: str = (
         "redis://aboba:RvPtXZyLRxRd7zvj3mieYvXAQAQH9Cjw@172.20.0.3:6379/0"
     )
 
-    task_serializer = "json"
-    result_serializer = "json"
-    accept_content = ["json"]  # noqa:RUF012
-    timezone = "UTC"
-    enable_utc = True
+    task_serializer: str = "json"
+    result_serializer: str = "json"
+    accept_content: list[str] = ["json"]  # noqa:RUF008
+    timezone: str = "UTC"
+    enable_utc: bool = True
