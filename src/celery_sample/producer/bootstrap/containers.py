22c642023ff7b6342c09d253ae1fc6148bc9344f
admin-313 <w@quantum-level.net>
2025-06-27 11:22:35 +0300

    pass celery config as kwargs

--- src/celery_sample/producer/bootstrap/containers.py
+++ src/celery_sample/producer/bootstrap/containers.py
@@ -1,18 +1,18 @@
+from dataclasses import asdict
+
 from celery import Celery
 from dependency_injector import containers
 from dependency_injector.providers import Factory, Singleton
 
 from producer.actions.send_message import SendMessage
+from producer.celeryconfig import CeleryConfig
 
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["producer.router"])
 
     celery: Singleton[Celery] = Singleton(
-        Celery,
-        main="producer",
-        broker="pyamqp://guest:guest@172.20.0.4:5672//",
-        backend="redis://aboba:RvPtXZyLRxRd7zvj3mieYvXAQAQH9Cjw@172.20.0.3:6379/0",
+        Celery, main="producer", **asdict(CeleryConfig()),
     )
 
     send_message: Factory[SendMessage] = Factory(SendMessage, celery=celery)
