606d8e0fe7a12f220182bb54a0a3b78a415dde5a
admin-313 <w@quantum-level.net>
2025-06-27 13:27:12 +0300

    add override decorator

--- src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
+++ src/celery_sample/consumer/bootstrap/heavy_lifting_request.py
@@ -1,4 +1,5 @@
 import logging
+from typing import override
 
 from celery import Task
 from celery.worker.request import Request
@@ -7,11 +8,12 @@ logger = logging.getLogger(__name__)
 
 
 class HeavyLiftingRequest(Request):
+    @override
     def on_failure(
         self,
         exc_info,  # noqa: ANN001
-        send_failed_event: bool = True,  # noqa: FBT001, FBT002
-        return_ok: bool = False,  # noqa: FBT001, FBT002
+        send_failed_event: bool = True,
+        return_ok: bool = False,
     ) -> None:
         super().on_failure(exc_info, send_failed_event, return_ok)
         logger.error("The task %s has been failed", self.task.name)
