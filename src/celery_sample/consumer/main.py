b40f79a77c249f26e70250a0368cbaec1c0b5051
admin-313 <w@quantum-level.net>
2025-06-30 17:35:46 +0300

    add cpu cound task

--- src/celery_sample/consumer/main.py
+++ src/celery_sample/consumer/main.py
@@ -2,6 +2,10 @@ import logging
 
 from dependency_injector.wiring import Provide, inject
 
+from consumer.actions.cpu_heavy_lifting import (
+    CPUHeavyLifting,
+    CPUHeavyLiftingRequest,
+)
 from consumer.actions.io_heavy_lifting import (
     IOHeavyLifting,
     IOHeavyLiftingRequest,
@@ -27,11 +31,32 @@ celery = consumer_container.celery()
     worker_deduplicate_successful_tasks=False,
 )
 @inject
-def run_heavy_lifting(
+def run_io_heavy_lifting(
     data: str,
     heavy_lifting: IOHeavyLifting = Provide[
         ConsumerContainer.io_heavy_lifting_action
     ],
 ) -> None:
     input_ = IOHeavyLiftingRequest(message=data)
+    heavy_lifting(data=input_)
+
+
+@celery.task(
+    base=HeavyLiftingTask,
+    name="test.cpu_bound",
+    acks_late=True,
+    task_acks_on_failure_or_timeout=True,
+    autoretry_for=(ValueError,),
+    retry_kwargs={"max_retries": 3, "countdown": 2},
+    worker_concurrency=12,
+    worker_deduplicate_successful_tasks=False,
+)
+@inject
+def run_cpu_heavy_lifting(
+    data: str,
+    heavy_lifting: CPUHeavyLifting = Provide[
+        ConsumerContainer.cpu_heavy_lifting_action
+    ],
+) -> None:
+    input_ = CPUHeavyLiftingRequest(message=data)
     heavy_lifting(data=input_)
