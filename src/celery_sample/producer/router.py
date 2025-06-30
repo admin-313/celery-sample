b12b564e94835b560f5e2f1cd9c10ac3ccdf4803
admin-313 <w@quantum-level.net>
2025-06-30 15:48:48 +0300

    add cpu_bound task handler

--- src/celery_sample/producer/router.py
+++ src/celery_sample/producer/router.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
+from producer.actions.cpu_bound import CPUBound, CPUBoundRequest
 from producer.actions.send_message import SendMessage, SendMessageRequest
 from producer.bootstrap.containers import Container
 
@@ -15,13 +16,25 @@ class ProducerRouterPost(BaseModel):
     message: str = Field(default="None message provided", alias="message")
 
 
-@producer_router.post("/")
+@producer_router.post("/io")
 @inject
 async def post_message(
     request: ProducerRouterPost,
     action: Annotated[SendMessage, Depends(Provide[Container.send_message])],
 ) -> JSONResponse:
     action_data = SendMessageRequest(message=request.message)
+    task_id = await action(data=action_data)
+
+    return JSONResponse(content={"task_id": task_id}, status_code=202)
+
+
+@producer_router.post("/cpu")
+@inject
+async def post_cpu_bound(
+    request: ProducerRouterPost,
+    action: Annotated[CPUBound, Depends(Provide[Container.cpu_bound])],
+) -> JSONResponse:
+    action_data = CPUBoundRequest(message=request.message)
     task_id = await action(data=action_data)
 
     return JSONResponse(content={"task_id": task_id}, status_code=202)
